e139f969dc99fddf353ba3d99d794aab93461ef6
Adithya P Binu <145443401+adithya59@users.noreply.github.com>
2025-02-17 19:19:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,3 +16,31 @@ app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+import e from "express";
+import { MongoURL, PORT } from "./config.js";
+import mongoose from "mongoose";
+import petRouter from "./routes/pets.route.js";
+
+const app = e()
+
+//Middleware
+app.use(e.json())
+app.use('/pets', petRouter)
+
+//Routes
+app.get('/', (req, res) => {
+  return res.status(201).send('Welcome to Petverse')
+})
+
+//Connection to DB
+mongoose.connect(MongoURL)
+  .then(() => {
+    console.log('Connection Successful.')
+    app.listen(PORT, () => {
+      console.log(`App is listening at: http://localhost:${PORT}`)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
