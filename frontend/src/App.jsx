229e2d766fcb848e75e8a8d18e7c62a15d24991a
Doney <doney2k3@gmail.com>
2025-02-24 21:58:54 +0530

    Updated PETVERSE project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import LoginPage from "./components/Loginpage";  // Ensure correct casing
+import SignupPage from "./components/SignupPage"; // Import Signup Page
 
 function App() {
   return (
     <div>
       <h1>Welcome to PETVERSE</h1>  {/* Add this to check if anything appears */}
       <LoginPage />
+      <SignupPage/>
     </div>
   );
 }
 
-export default App;
+export default App; 
