307441c018d9deed9b2fb500ccb77fb4a4cfbb3b
Adithya59 <adithyapbinu2026@cs.sjcetpalai.ac.in>
2025-03-02 10:59:40 +0530

    User

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -4,33 +4,51 @@ import User from "../models/user.model.js";
 
 //SignUp
 const signUp = async (req, res) => {
-    const { name, email, password, role } = req.body;
-    try {
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-        const newUser = new User({ name, email, password: hashedPassword, role });
-        await newUser.save();
-        res.status(201).json({ message: "User registered successfully" });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
-}
+  const { name, email, password, role } = req.body;
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({ name, email, password: hashedPassword, role });
+    await newUser.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 
 //Login
 const login = async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        const user = await User.findOne({ email });
-        if (!user) return res.status(400).json({ error: "User not found" });
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ error: "User not found" });
 
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) return res.status(400).json({ error: "Invalid credentials" });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ error: "Invalid credentials" });
 
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
-        res.json({ token, user: { id: user._id, name: user.name, email: user.email, role: user.role } });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+
+    if (user.role === "buyer") {
+      res.redirect("/buyer-dashboard");
+    } else if (user.role === "seller") {
+      res.redirect("/seller-dashboard");
+    } else if (user.role === "admin") {
+      res.redirect("/admin-dashboard");
     }
-}
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 
-export { signUp, login }
+export { signUp, login };
