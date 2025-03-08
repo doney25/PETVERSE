9116d5e47d19780315896e37ec6681439989a4de
Adithya P Binu <adithyapbinu2026@cs.sjcetpalai.ac.in>
2025-03-09 00:33:28 +0530

    Sellers Can now list pets

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import userRouter from "./routes/user.route.js"
 import dotenv from 'dotenv';
 import http from 'http';
 import { Server } from 'socket.io';
+import path from "path";
+import multer from 'multer'
 
 const app = express()
 const server = http.createServer(app);
@@ -13,14 +15,39 @@ const io = new Server(server)
 
 //Middleware
 app.use(express.json())
-app.use(cors({
-    origin: 'http://localhost:5173',  
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true  
-}))
+app.use(cors())
+// app.use(cors({
+//     origin: 'http://localhost:5173',  
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true  
+// }))
 app.use('/pets', petRouter)
 app.use('/api/users', userRouter);
+app.use('/uploads', express.static('uploads'));
 dotenv.config();
+
+//multer storage engine for images
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Store images in the 'uploads' folder
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname)); // Use a unique filename
+  },
+});
+
+const upload = multer({ storage: storage });
+
+
+// Image upload route
+app.post('/uploadImage', upload.array('images', 10), (req, res) => {
+  if (req.files && req.files.length > 0) {
+    const imageUrls = req.files.map(file => `/uploads/${file.filename}`); // Store relative path
+    res.json({ imageUrls });
+  } else {
+    res.status(400).json({ message: 'No image files uploaded' });
+  }
+});
 
 const PORT = process.env.PORT ;
 
